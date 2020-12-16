479ba1a260095c94ac51ac9c0971c9e811740f75
Oksana <solomkao@gmail.com>
2020-12-16 13:28:14 +0200

    Init commit

--- src/com/solomka/Main.java
+++ src/com/solomka/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Attempt #1
-        //throw new ("The exception was generated deliberately");
+        //throw new OutOfMemoryError ("The exception was generated deliberately");
 
         // Attempt #2
         String str = "Simple string ";
