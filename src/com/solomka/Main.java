2f81333301c554bfce3c3c36894d75ccb4c84a9a
Oksana <solomkao@gmail.com>
2020-12-16 13:14:53 +0200

    Init commit

---
+++ src/com/solomka/Main.java
@@ -0,0 +1,16 @@
+package com.solomka;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Attempt #1
+        //throw new ("The exception was generated deliberately");
+
+        // Attempt #2
+        String str = "Simple string ";
+        while(true){
+            str += str;
+        }
+    }
+}
